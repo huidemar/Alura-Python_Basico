7302ccee492ea180274c5312668ce92e4e380f79
Huidemar Costa <huidemar@gmail.com>
2018-11-20 21:59:55 -0200

    Aula 3
    Testando valores

--- adivinhacao.py
+++ adivinhacao.py
@@ -1,16 +1,27 @@
 print("*********************************")
+print("Meu primeiro programa P Y T H O N")
+print("*********************************")
 print("Bem vindo ao jogo de Adivinhação!")
 print("*********************************")
 
 numero_secreto = 42
 
-chute_str = input("Digite o seu número: ")
-print("Você digitou: ", chute_str)
-chute = int(chute_str)
+chute = input("Digite o seu número: ")
 
-if (numero_secreto == chute):
+print("Você digitou ", chute)
+
+chute = int(chute)
+
+acertou = chute == numero_secreto
+maior   = chute > numero_secreto
+menor   = chute < numero_secreto
+
+if (acertou):
     print("Você acertou!")
 else:
-    print("Você errou!")
+    if(maior):
+        print("Você errou! O seu chute foi MAIOR do que o número secreto.")
+    elif(menor):
+        print("Você errou! O seu chute foi MENOR do que o número secreto.")
 
-print("Fim do jogo")
+print("Fim do jogo!!!!!!")
