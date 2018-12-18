3adf81705c2f97fe8f7c77886daa8704e231d998
Huidemar Costa <huidemar@gmail.com>
2018-12-17 23:15:21 -0200

    Add files via upload

--- adivinhacao.py
+++ adivinhacao.py
@@ -8,19 +8,12 @@ def jogar():
     total_de_tentativas = 3
     pontos = 100
 
-    print("Qual o nível de dificuldade ?")
-    print("(1)Fácil (2)Médio (3)Difícil")
+    nivel = carrega_nivel_dificuldade()
 
-    nivel=int(input("Define o nível: "))
+    total_de_tentativas = inicializa_total_de_tentativas(nivel)
 
-    if (nivel == 1):
-        total_de_tentativas = 20
-    elif (nivel == 2):
-        total_de_tentativas = 10
-    elif (nivel == 3):
-        total_de_tentativas = 5
 
-    for rodada in range(1,total_de_tentativas + 1):
+    for rodada in range(1, total_de_tentativas + 1):
         print("Tentativa {} de {}".format(rodada, total_de_tentativas))
         chute_str = input("Digite um número entre 1 e 100: ")
         print("Você digitou ", chute_str)
@@ -31,16 +24,16 @@ def jogar():
             continue
 
         acertou = chute == numero_secreto
-        maior   = chute > numero_secreto
-        menor   = chute < numero_secreto
+        maior = chute > numero_secreto
+        menor = chute < numero_secreto
 
         if (acertou):
             print("Você acertou!")
             break
         else:
-            if(maior):
+            if (maior):
                 print("Você errou! O seu chute foi MAIOR do que o número secreto.")
-            elif(menor):
+            elif (menor):
                 print("Você errou! O seu chute foi MENOR do que o número secreto.")
 
             pontos_perdidos = abs(numero_secreto - chute)
@@ -54,6 +47,21 @@ def imprime_titulo():
     print("*********************************")
     print("Bem vindo ao jogo de Adivinhação!")
     print("*********************************")
+
+
+def carrega_nivel_dificuldade():
+    print("Qual o nível de dificuldade ?")
+    print("(1)Fácil (2)Médio (3)Difícil")
+    return int(input("Define o nível: "))
+
+
+def inicializa_total_de_tentativas(nivel):
+    if (nivel == 1):
+        return 20
+    elif (nivel == 2):
+        return 10
+    elif (nivel == 3):
+        return 5
 
 
 if (__name__ == "__main__"):
